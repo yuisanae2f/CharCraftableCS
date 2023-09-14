34dd14298b970a50b1bb851e6c8ca227a909ce50
Yui San ae2f <84163330+YuiSanae2f@users.noreply.github.com>
2023-09-15 02:16:36 +0900

    .

--- doc/Craftable.js
+++ doc/Craftable.js
@@ -1,3 +1,8 @@
 strArr = [
-    {id: "translate", ko: "언어", en: "Languages"}
+    {id: "translate", ko: "언어", en: "Languages"},
+    {
+        id: "T[0]",
+        ko: '<a href="#T" class="generic">해당 자료형</a>은 <a href="https://yuisanae2f.github.io/YuiSanae2f/dictionary/class/interface" class="mark">이 인터페이스</a>가 값을 문자로 받는가, 문자열로 받는가를 결정합니다.',
+        en: '<a href="#T" class="generic">Given Data Type</a> will determine whether <a href="https://yuisanae2f.github.io/YuiSanae2f/dictionary/class/interface" class="mark">this interface</a> gets the value as <a href="https://yuisanae2f.github.io/YuiSanae2f/dictionary/dataType/integer#char" class="reservedKey">char</a>, or <a href="https://yuisanae2f.github.io/YuiSanae2f/dictionary/pointer/array/string" class=reservedKey>string</a>.'
+    }
 ];
